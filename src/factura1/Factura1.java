a00e16b2d4fae68f7dccf70795aded4282b2e3de
nohemi <anaguilarm@alumno.uspg.edu.gt>
2020-10-18 12:21:19 -0500

    Agregue factura detalle

---
+++ src/factura1/Factura1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factura1;
+
+/**
+ * Ana NOhemi Aguilar Martinez 2000316
+ * @author Nohemi Aguilar
+ */
+public class Factura1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
